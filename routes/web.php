7ce9190102dd8be6a84f0a4299e52c8d96a642b1
ahmettcetinn <ahmetmercimekmaster@gmail.com>
2026-05-12 12:36:47 +0200

    Beveiliging gefixt: gebruikersprofiel vereist nu login

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,21 @@ Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-// ← NIEUW: Registratie route!
 Route::get('/register', [LoginController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [LoginController::class, 'register']);
-// Gebruiker profiel bekijken (docent mag iedereen, student alleen zichzelf)
-Route::get('/gebruiker/{id}', [LoginController::class, 'profiel'])->name('gebruiker.profiel');
+
 // Documentatie pagina - publiek toegankelijk
 Route::get('/documentatie', function () {
     return view('documentatie');
 })->name('documentatie');
 
+// ====================
+// BEVEILIGDE ROUTES (login verplicht)
+// ====================
 Route::middleware(['auth'])->group(function () {
+    // FIX: gebruiker profiel binnen auth (was publiek toegankelijk)
+    Route::get('/gebruiker/{id}', [LoginController::class, 'profiel'])->name('gebruiker.profiel');
+
     Route::get('/', [ProductController::class, 'index'])->name('producten.index');
     Route::get('/product/{id}/detail', [ProductController::class, 'detail'])->name('producten.detail');
     Route::get('/product/create', [ProductController::class, 'create'])->name('producten.create');
@@ -35,6 +39,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/reservering/create', [ReserveringController::class, 'create'])->name('reserveringen.create');
     Route::post('/reservering', [ReserveringController::class, 'store'])->name('reserveringen.store');
     Route::delete('/reservering/{id}', [ReserveringController::class, 'destroy'])->name('reserveringen.destroy');
-    // Mijn Account - Eigen reserveringen zien
+
     Route::get('/mijn-account', [ReserveringController::class, 'mijnAccount'])->name('mijn.account');
 });
